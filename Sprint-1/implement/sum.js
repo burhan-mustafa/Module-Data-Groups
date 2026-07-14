cd4c7e4ba533129a2bcc15088f51961568e31dac
burhan-mustafa <m-b.mustafa@outlook.com>
2026-07-14 22:54:51 +0100

    completed sum function

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,10 @@
 function sum(elements) {
+  const sortedArr = elements.filter((item) => typeof item === "number");
+  if (sortedArr.length === 0) {
+    return 0;
+  }
+  const total = sortedArr.reduce((total, num) => total + num, null);
+  return total;
 }
 
 module.exports = sum;
