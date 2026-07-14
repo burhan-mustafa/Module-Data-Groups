bdd481228742a6395599026d3441e0d9d8bc3beb
burhan-mustafa <m-b.mustafa@outlook.com>
2026-07-14 21:02:38 +0100

    wrote all tests for max.js

--- Sprint-1/implement/max.test.js
+++ Sprint-1/implement/max.test.js
@@ -16,28 +16,57 @@ const findMax = require("./max.js");
 // When passed to the max function
 // Then it should return -Infinity
 // Delete this test.todo and replace it with a test.
-test.todo("given an empty array, returns -Infinity");
+test("given an empty array, returns -Infinity", () => {
+  const arr = [];
+  expect(max(arr)).toEqual(-Infinity);
+});
 
 // Given an array with one number
 // When passed to the max function
 // Then it should return that number
+test("given an array with one number, returns that same number", () => {
+  const arr = [1];
+  expect(max(arr)).toEqual(1);
+});
 
 // Given an array with both positive and negative numbers
 // When passed to the max function
 // Then it should return the largest number overall
+test("given an array with both - and + values, returns the largest number overall", () => {
+  const arr = [-4, 2];
+  expect(max(arr)).toEqual(-4);
+});
 
 // Given an array with just negative numbers
 // When passed to the max function
 // Then it should return the closest one to zero
+test("given an empty array, returns -Infinity", () => {
+  const arr = [-10, -20, -5];
+  expect(max(arr)).toEqual(-5);
+});
 
 // Given an array with decimal numbers
 // When passed to the max function
 // Then it should return the largest decimal number
+test("given an empty array, returns 5-Infinity", () => {
+  const arr = [0.6, 0.89];
+  expect(max(arr)).toEqual(0.89);
+});
 
 // Given an array with non-number values
 // When passed to the max function
 // Then it should return the max and ignore non-numeric values
+test("given an array with non-number values, returns the largest number", () => {
+  const arr = [3, "hello", 8, true, 5];
+
+  expect(max(arr)).toEqual(8);
+});
 
 // Given an array with only non-number values
 // When passed to the max function
 // Then it should return the least surprising value given how it behaves for all other inputs
+test("given an array with only non-number values, returns -Infinity", () => {
+  const arr = ["hello", true, null, undefined];
+
+  expect(max(arr)).toEqual(-Infinity);
+});
