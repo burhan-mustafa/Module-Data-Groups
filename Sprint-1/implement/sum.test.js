68e0325761b00282239c3bc6504d259de47490c7
burhan-mustafa <m-b.mustafa@outlook.com>
2026-07-14 22:27:18 +0100

    tests completed

--- Sprint-1/implement/sum.test.js
+++ Sprint-1/implement/sum.test.js
@@ -3,34 +3,61 @@
 In this kata, you will need to implement a function that sums the numerical elements of an array
 
 E.g. sum([10, 20, 30]), target output: 60
-E.g. sum(['hey', 10, 'hi', 60, 10]), target output: 80 (ignore any non-numerical elements)
+E.g. sum(["hey", 10, "hi", 60, 10]), target output: 80
 */
 
 const sum = require("./sum.js");
 
-// Acceptance Criteria:
-
 // Given an empty array
 // When passed to the sum function
 // Then it should return 0
-test.todo("given an empty array, returns 0")
+test("given an empty array, returns 0", () => {
+  const arr = [];
+
+  expect(sum(arr)).toEqual(0);
+});
 
 // Given an array with just one number
 // When passed to the sum function
 // Then it should return that number
+test("given an array with one number, returns that number", () => {
+  const arr = [10];
+
+  expect(sum(arr)).toEqual(10);
+});
 
 // Given an array containing negative numbers
 // When passed to the sum function
 // Then it should still return the correct total sum
+test("given an array containing negative numbers, returns the correct total", () => {
+  const arr = [-10, -5, 20];
+
+  expect(sum(arr)).toEqual(5);
+});
 
 // Given an array with decimal/float numbers
 // When passed to the sum function
 // Then it should return the correct total sum
+test("given an array containing decimal numbers, returns the correct total", () => {
+  const arr = [1.5, 2.5, 3];
+
+  expect(sum(arr)).toEqual(7);
+});
 
 // Given an array containing non-number values
 // When passed to the sum function
 // Then it should ignore the non-numerical values and return the sum of the numerical elements
+test("given an array containing non-number values, ignores them and returns the correct total", () => {
+  const arr = ["hello", 10, true, 20, null];
+
+  expect(sum(arr)).toEqual(30);
+});
 
 // Given an array with only non-number values
 // When passed to the sum function
 // Then it should return the least surprising value given how it behaves for all other inputs
+test("given an array containing only non-number values, returns 0", () => {
+  const arr = ["hello", true, null, undefined];
+
+  expect(sum(arr)).toEqual(0);
+});
