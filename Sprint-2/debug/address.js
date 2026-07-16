1105f02214b8527f1290ebd8e4c17db524c20b6a
burhan-mustafa <m-b.mustafa@outlook.com>
2026-07-16 20:00:26 +0100

    completed address.js

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,5 +1,7 @@
 // Predict and explain first...
 
+// I would expect the console.log to show the house number is undefined as the object address has not properly referenced the key house number.
+
 // This code should log out the houseNumber from the address object
 // but it isn't working...
 // Fix anything that isn't working
@@ -12,4 +14,4 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address["houseNumber"]}`);
