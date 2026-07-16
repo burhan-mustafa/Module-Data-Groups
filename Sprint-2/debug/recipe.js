8a8114a4d1c6997f354adc23afb990167a66e6a8
burhan-mustafa <m-b.mustafa@outlook.com>
2026-07-16 22:56:13 +0100

    completed recipe.js

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,5 +1,9 @@
 // Predict and explain first...
 
+// The console log will display the title and how many people the recipe serves but it will not display the
+// ingredients correctly because we have not accessed the `ingredients` property. Instead, `${recipe}` refers
+// to the entire object.
+
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
 // How can you fix it?
@@ -11,5 +15,5 @@ const recipe = {
 };
 
 console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+ingredients:
+${recipe.ingredients.join("\n")}`);
