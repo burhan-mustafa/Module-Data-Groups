7ff65a4ce355d7e033e5905f3dc925bae9858b88
burhan-mustafa <m-b.mustafa@outlook.com>
2026-07-20 01:44:02 +0100

    completed querystring

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,13 +1,38 @@
 function parseQueryString(queryString) {
   const queryParams = {};
+
   if (queryString.length === 0) {
     return queryParams;
   }
-  const keyValuePairs = queryString.split("&");
+
+  const keyValuePairs = queryString.split("&").filter((item) => item !== "");
 
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
+    const startOfQuery = pair.indexOf("=");
+
+    let key;
+    let value;
+
+    if (startOfQuery === -1) {
+      key = pair;
+      value = "";
+    } else {
+      key = pair.slice(0, startOfQuery);
+      value = pair.slice(startOfQuery + 1);
+    }
+
+    key = decodeURIComponent(key.replaceAll("+", " "));
+    value = decodeURIComponent(value.replaceAll("+", " "));
+
+    if (Object.hasOwn(queryParams, key)) {
+      if (Array.isArray(queryParams[key])) {
+        queryParams[key].push(value);
+      } else {
+        queryParams[key] = [queryParams[key], value];
+      }
+    } else {
+      queryParams[key] = value;
+    }
   }
 
   return queryParams;
